633ae829f6febbca9c71bcf1faa8a371e5c295ec
Yim-HaEun <gkdms2710@naver.com>
2024-09-08 02:12:30 +0900

    TableViewCell

--- HelloWorldApp/TableViewCell.swift
+++ HelloWorldApp/TableViewCell.swift
@@ -9,4 +9,5 @@ import UIKit
 
 class TableViewCell : UITableViewCell{
     
+    @IBOutlet var lblNo: UILabel!
 }
--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -7,14 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //테이블뷰에서 표현해야 할 행의 개수를 나타냄
+        return self.arrNo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //
+        let cell = self.TblList.dequeueReusableCell(withIdentifier: "TableViewCell") as!
+        TableViewCell
+        cell.lblNo.text = String(self.arrNo[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        
+    }
+    
  
    @IBOutlet var lblHello: UILabel!
     
     @IBOutlet var txtName: UITextField!
     
+    @IBOutlet var TblList: UITableView!
+    //추가된 코드
+    var arrNo:[Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //추가된 코드
+        self.TblList.delegate = self
+        self.TblList.dataSource = self
+        for index in 1...10{
+            self.arrNo.append(index)
+        }
+       
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
